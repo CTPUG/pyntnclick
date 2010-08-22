705bd0eae2120757463ce8af169ca3f9197b66e0
Jeremy Thurgood <firxen@gmail.com>
2010-08-22 18:09:25 +0200

    Add a game screen. So far, the game content looks a *lot* like the main menu.

--- gamelib/gamescreen.py
+++ gamelib/gamescreen.py
@@ -6,11 +6,11 @@ from albow.screen import Screen
 from albow.controls import Button, Label
 from albow.layout import Column
 
-class MenuScreen(Screen):
+class GameScreen(Screen):
     def __init__(self, shell):
         Screen.__init__(self, shell)
         self.shell = shell
-        StartButton = Button('Start New Game', action = self.start)
+        StartButton = Button('Main Menu', action = self.main_menu)
         QuitButton = Button('Quit', action = shell.quit)
         Title = Label('Caught! ... In SPAACE')
         menu = Column([
@@ -20,8 +20,8 @@ class MenuScreen(Screen):
             ], align='l', spacing=20)
         self.add_centered(menu)
 
-    def start(self):
-        print 'Starting the game'
-        self.shell.show_screen(self.shell.game_screen)
+    def main_menu(self):
+        print 'Returning to menu'
+        self.shell.show_screen(self.shell.menu_screen)
 
 
--- gamelib/main.py
+++ gamelib/main.py
@@ -11,12 +11,14 @@ from pygame.locals import SWSURFACE, SRCALPHA
 from albow.dialogs import alert
 from albow.shell import Shell
 from menu import MenuScreen
+from gamescreen import GameScreen
 from constants import SCREEN
 
 class MainShell(Shell):
     def __init__(self, display):
         Shell.__init__(self, display)
         self.menu_screen = MenuScreen(self)
+        self.game_screen = GameScreen(self)
         self.show_screen(self.menu_screen)
 
 def main():
