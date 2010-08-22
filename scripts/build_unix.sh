6ae56731b55fbceb1860918fdce50ff3a2efcfa7
Jeremy Thurgood <firxen@gmail.com>
2010-08-22 15:32:08 +0200

    Some "build" scripts.

---
+++ scripts/build_unix.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GAME_NAME="captured"
+
+mkdir -p build/${GAME_NAME} dist
+
+cp -r run_game.py gamelib data build/${GAME_NAME}
+
+cd build
+
+# Add albow to our game
+unzip ../deps/Albow-2.1.0.zip
+mv Albow-2.1.0/albow ${GAME_NAME}
+rm -rf Albow-2.1.0
+
+tar czf ../dist/${GAME_NAME}.tgz captured
+
