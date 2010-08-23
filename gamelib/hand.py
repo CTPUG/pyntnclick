f197f034e4db0b54f3750540c495af86f4068fd7
Simon Cross <hodgestar@gmail.com>
2010-08-23 10:35:53 +0200

    Correct out-by-one indentation error. :)

--- gamelib/hand.py
+++ gamelib/hand.py
@@ -6,28 +6,28 @@ from albow.utils import frame_rect
 from pygame.color import Color
 
 class HandButton(ImageButton):
-   """The fancy hand button for the widget"""
+    """The fancy hand button for the widget"""
 
-   sel_colour = Color('red')
-   sel_width = 2
+    sel_colour = Color('red')
+    sel_width = 2
 
-   def __init__(self, action):
-      # FIXME: Yes, please.
-      this_image = get_image('items', 'square.png')
-      ImageButton.__init__(self, image=this_image, action=action)
-      self.selected = False # Flag if we're selected
+    def __init__(self, action):
+        # FIXME: Yes, please.
+        this_image = get_image('items', 'square.png')
+        ImageButton.__init__(self, image=this_image, action=action)
+        self.selected = False # Flag if we're selected
 
-   def draw(self, surface):
-      """Draw the widget"""
-      print 'drawing widget', self.selected
-      ImageButton.draw(self, surface)
-      if self.selected:
-         rect = surface.get_rect().inflate(-self.sel_width, -self.sel_width)
-         frame_rect(surface, self.sel_colour, rect, self.sel_width)
+    def draw(self, surface):
+        """Draw the widget"""
+        print 'drawing widget', self.selected
+        ImageButton.draw(self, surface)
+        if self.selected:
+            rect = surface.get_rect().inflate(-self.sel_width, -self.sel_width)
+            frame_rect(surface, self.sel_colour, rect, self.sel_width)
 
-   def toggle_selected(self):
-      self.selected = not self.selected
+    def toggle_selected(self):
+        self.selected = not self.selected
 
-   def unselect(self):
-       self.selected = False
+    def unselect(self):
+        self.selected = False
 
