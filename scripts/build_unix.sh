ff8a3bfe44996cad32eaed74ebbd8eed21d4d7de
Jeremy Thurgood <firxen@gmail.com>
2010-08-23 17:32:01 +0200

    Minor build script fixes.

--- scripts/build_unix.sh
+++ scripts/build_unix.sh
@@ -4,12 +4,12 @@ GAME_NAME="captured"
 
 mkdir -p build/${GAME_NAME} dist
 
-cp -r README.txt run_game.py gamelib tests Resources build/${GAME_NAME}
+cp -r README.txt run_game.py gamelib Resources build/${GAME_NAME}
 
 cd build
 
 # Add albow to our game
-unzip ../deps/Albow-2.1.0.zip
+unzip ../deps/Albow-2.1.0.zip > /dev/null
 mv Albow-2.1.0/albow ${GAME_NAME}
 rm -rf Albow-2.1.0
 
