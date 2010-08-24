a5f1ab10e5b6f62f1330f0911754ebfef4edbd61
Neil Muller <drnlmuller@gmail.com>
2010-08-25 00:32:44 +0200

    Make hand activate on mouse down, like inventory

--- gamelib/hand.py
+++ gamelib/hand.py
@@ -2,19 +2,21 @@
 
 from constants import BUTTON_SIZE
 
-from albow.controls import ImageButton
+from albow.controls import Image
 from albow.resource import get_image
-from albow.utils import frame_rect
-from pygame.color import Color
 from pygame.rect import Rect
 
 
-class HandButton(ImageButton):
+class HandButton(Image):
     """The fancy hand button for the widget"""
 
     def __init__(self, action):
         # FIXME: Yes, please.
         this_image = get_image('items', 'hand.png')
-        ImageButton.__init__(self, image=this_image, action=action)
+        Image.__init__(self, image=this_image)
+        self.action = action
         self.set_rect(Rect(0, 0, BUTTON_SIZE, BUTTON_SIZE))
+
+    def mouse_down(self, event):
+        self.action()
 
