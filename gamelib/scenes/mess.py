09a4e38965bf7f983e9c80bf819e317d3aac4b03
Simon Cross <hodgestar@gmail.com>
2010-08-24 22:59:46 +0200

    Add items to mess.

--- gamelib/scenes/mess.py
+++ gamelib/scenes/mess.py
@@ -1,6 +1,7 @@
 """Mess where crew eat. Fun stuff."""
 
 from gamelib.state import Scene, Item, Thing, InteractImage, InteractNoImage
+from gamelib.cursor import CursorSprite
 
 
 class Mess(Scene):
@@ -14,8 +15,40 @@ class Mess(Scene):
 
     def __init__(self, state):
         super(Mess, self).__init__(state)
+        self.add_item(EmptyCan("empty_can"))
+        self.add_item(FullCan("full_can"))
+        self.add_item(TubeFragments("tube_fragments"))
+        self.add_item(ReplacementTubes("replacement_tubes"))
         self.add_thing(CansOnShelf())
         self.add_thing(Tubes())
+
+
+class EmptyCan(Item):
+    "After emptying the full can."
+
+    INVENTORY_IMAGE = "empty_can.png"
+    CURSOR = CursorSprite('empty_can_cursor.png', 47, 3)
+
+
+class FullCan(Item):
+    "Found on the shelf."
+
+    INVENTORY_IMAGE = "full_can.png"
+    CURSOR = CursorSprite('full_can_cursor.png', 47, 3)
+
+
+class TubeFragments(Item):
+    "Old tubes that need repair."
+
+    INVENTORY_IMAGE = "tube_fragments.png"
+    CURSOR = CursorSprite('tube_fragments_cursor.png', 47, 3)
+
+
+class ReplacementTubes(Item):
+    "Repaired tubes."
+
+    INVENTORY_IMAGE = "replacement_tubes.png"
+    CURSOR = CursorSprite('replacement_tubes.png', 47, 3)
 
 
 class CansOnShelf(Thing):
