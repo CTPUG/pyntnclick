b6dd39064e5498506e5702c8b44d1be264faeb5a
Stefano Rivera <stefano@rivera.za.net>
2010-08-26 21:51:27 +0200

    Things and Items for bridge

--- gamelib/scenes/bridge.py
+++ gamelib/scenes/bridge.py
@@ -1,7 +1,7 @@
 """Bridge where the final showdown with the AI occurs."""
 
+from gamelib.cursor import CursorSprite
 from gamelib.state import Scene, Item, Thing, Result, InteractText
-
 
 class Bridge(Scene):
 
@@ -14,7 +14,11 @@ class Bridge(Scene):
 
     def __init__(self, state):
         super(Bridge, self).__init__(state)
+        self.add_item(Superconductor('superconductor'))
+        self.add_item(Stethoscope('stethoscope'))
         self.add_thing(ToMap())
+        self.add_thing(MassageChair())
+        self.add_thing(StethoscopeThing())
 
     def enter(self):
         return Result("The bridge is in a sorry, shabby state")
@@ -37,4 +41,93 @@ class ToMap(Thing):
         self.state.set_current_scene("map")
 
 
+class MassageChair(Thing):
+    "The captain's massage chair, contains superconductor"
+
+    NAME = 'bridge.massagechair'
+
+    INTERACTS = {
+        'chair': InteractText(200, 200, 'Chair'),
+    }
+
+    INITIAL = 'chair'
+
+    INITIAL_DATA = {
+            'contains_superconductor': True,
+    }
+
+    def interact_without(self):
+        return Result(detail_view='chair_detail')
+
+    def get_description(self):
+        if self.get_data('contains_superconductor'):
+            return "A top of the line Massage-o-Matic Captain's Executive Command Chair."
+        return "The chair won't work any more, it has no power."
+
+
+class Stethoscope(Item):
+    "Used for cracking safes. Found on the doctor on the chair"
+
+    INVENTORY_IMAGE = 'triangle.png'
+    CURSOR = CursorSprite('triangle.png', 20, 30)
+
+
+class StethoscopeThing(Thing):
+    "Stehoscope on the doctor"
+
+    NAME ='bridge.stethoscope'
+
+    INTERACTS = {
+        'stethoscope': InteractText(300, 200, 'Stethoscope'),
+    }
+
+    INITIAL = 'stethoscope'
+
+    def interact_without(self):
+        self.state.add_inventory_item('stethoscope')
+        self.scene.remove_thing(self)
+        return Result("You pick up the stethoscope and verify that the doctor's "
+                      "heart has stoped. Probably a while ago.")
+
+
+class Superconductor(Item):
+    "Used for connecting high-powered parts of the ship up"
+
+    INVENTORY_IMAGE = 'triangle.png'
+    CURSOR = CursorSprite('triangle.png', 20, 30)
+
+
+class SuperconductorThing(Thing):
+    "Superconductor from the massage chair."
+
+    NAME ='bridge.superconductor'
+
+    INTERACTS = {
+        'superconductor': InteractText(100, 200, 'Superconductor'),
+    }
+
+    INITIAL = 'superconductor'
+
+    def interact_without(self):
+        self.state.add_inventory_item('superconductor')
+        self.state.current_scene.things['bridge.massagechair'] \
+                          .set_data('contains_superconductor', False)
+        self.scene.remove_thing(self)
+        return Result("You pick up the stethoscope and verify that the doctor's "
+                      "heart has stoped. Probably a while ago.")
+
+class ChairDetail(Scene):
+
+    FOLDER = 'bridge'
+    BACKGROUND = 'chair_detail.png'
+    NAME = 'chair_detail'
+
+    SIZE = (300, 300)
+
+    def __init__(self, state):
+        super(ChairDetail, self).__init__(state)
+        self.add_thing(SuperconductorThing())
+
+
 SCENES = [Bridge]
+DETAIL_VIEWS = [ChairDetail]
