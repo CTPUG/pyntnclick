a09f7efb21be04430d02ebf97338db19374ce766
Neil Muller <drnlmuller@gmail.com>
2010-08-27 15:51:50 +0200

    Add length parameter

--- tools/gen_sound.py
+++ tools/gen_sound.py
@@ -1,6 +1,6 @@
 # Generate 'perfect' sine wave sounds
 
-# Design notes: produces ~= 0.5s raw CDDA audio - 44100 Hz, 16 bit signed values
+# Design notes: produces ~= (use requested) s raw CDDA audio - 44100 Hz, 16 bit signed values
 # Input is freq in Hz - 440 for A, etc. - must be an integer
 # Output is written the file beep<freq>.pcm
 # Convert to ogg with oggenc -r <file>
@@ -13,7 +13,7 @@ import struct
 CDDA_RATE = 44100
 MAX = 105*256 # Max value for sine wave
 
-def gen_sine(freq):
+def gen_sine(freq, secs):
     filename = 'beep%s.pcm' % freq
     # We need to generate freq cycles and sample that CDDA_RATE times
     samples_per_cycle = CDDA_RATE / freq
@@ -23,11 +23,15 @@ def gen_sine(freq):
         y = MAX * math.sin(rad)
         data.append(struct.pack('<i', y))
     output = open(filename, 'w')
-    for x in range(freq/2):
+    for x in range(int(freq * secs)):
         output.write(''.join(data))
     output.close()
 
 
 if __name__ == "__main__":
     freq = int(sys.argv[1])
-    gen_sine(freq)
+    if len(sys.argv) > 2:
+        secs = float(sys.argv[2])
+    else:
+        secs = 0.25
+    gen_sine(freq, secs)
