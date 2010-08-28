e2710e942e699c99e20c540bffdce695f387f258
Neil Muller <drnlmuller@gmail.com>
2010-08-28 21:29:49 +0200

    Add skeleton won screen

---
+++ gamelib/scenes/won.py
@@ -0,0 +1,28 @@
+"""You WON screen"""
+
+from gamelib.state import Scene, Item, Thing, Result
+from gamelib.scenes.game_constants import PLAYER_ID
+from gamelib.scenes.scene_widgets import (Door, InteractText, InteractNoImage,
+                                          InteractRectUnion, InteractImage,
+                                          InteractAnimated, GenericDescThing,
+                                          make_jim_dialog)
+
+
+class Won(Scene):
+
+    FOLDER = "won"
+    BACKGROUND = "won.png"
+
+    INITIAL_DATA = {
+        'accessible': False,
+        }
+
+    def __init__(self, state):
+        super(Won, self).__init__(state)
+
+    def enter(self):
+        return Result("Congratulations, Prisoner %s. You head off to your chosen "
+                "destination, safe in the knowledge that nothing can stop you. "
+                "Now you just need to do something during the intervening months" % PLAYER_ID)
+
+SCENES = [Won]
