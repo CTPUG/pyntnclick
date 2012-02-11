edb0905984000b5c2f5c65362233b7d0b4899419
Jeremy Thurgood <firxen@gmail.com>
2012-02-11 20:11:58 +0200

    ImageButtons have sizes.

--- pyntnclick/widgets/imagebutton.py
+++ pyntnclick/widgets/imagebutton.py
@@ -1,3 +1,5 @@
+import pygame
+
 from pyntnclick.widgets.base import Button
 
 
@@ -5,6 +7,8 @@ class ImageButtonWidget(Button):
     """An image that is also a button. Whatever next?"""
 
     def __init__(self, rect, image):
+        if not isinstance(rect, pygame.Rect):
+            rect = pygame.Rect(rect, image.get_size())
         super(ImageButtonWidget, self).__init__(rect)
         self.image = image
 
