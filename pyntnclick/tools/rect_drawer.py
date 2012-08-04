8a31e83b3c13804e0314baa757f70dae6758f55d
Neil Muller <drnlmuller@gmail.com>
2012-08-04 15:36:13 +0200

    Fix bug in zooming code

--- pyntnclick/tools/rect_drawer.py
+++ pyntnclick/tools/rect_drawer.py
@@ -289,8 +289,8 @@ class AppImage(Container):
             base_surface = surface.copy()
             self.do_unzoomed_draw(base_surface)
             zoomed = pygame.transform.scale(base_surface,
-                            (constants.zoom * constants.screen[0],
-                             constants.zoom * constants.screen[1]))
+                            (constants.zoom * base_surface.get_width(),
+                             constants.zoom * base_surface.get_height()))
             area = pygame.rect.Rect(self.zoom_offset[0], self.zoom_offset[1],
                                     self.zoom_offset[0] + constants.screen[0],
                                     self.zoom_offset[1] + constants.screen[1])
