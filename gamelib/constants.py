cbeff1372cf1254bc259f4fd7b75a162172a3fac
Stefano Rivera <stefano@rivera.za.net>
2013-01-26 18:54:34 +0200

    Translate the title (thanks Dmitrijs Ledkovs)

--- gamelib/constants.py
+++ gamelib/constants.py
@@ -1,7 +1,8 @@
 from pyntnclick.constants import GameConstants
+from pyntnclick.i18n import _
 
 
 class SSConstants(GameConstants):
-    title = 'Suspended Sentence'
+    title = _('Suspended Sentence')
     icon = 'suspended_sentence24x24.png'
     i18n_name = 'suspended-sentence'
